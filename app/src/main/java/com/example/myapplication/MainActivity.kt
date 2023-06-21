6d0d393a72fd98399afb52421749a35da0d79385
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-06-22 03:30:49 +0500

    Define a composable function

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,4 +15,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-
+@Composable
+fun MassageCart(name: String) {
+    Text(text = "Hello $name!")
+}

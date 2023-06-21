81a030e8ada6ad6c345403277a6e8d29b6f8a4b7
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-06-22 03:40:26 +0500

    Preview your function in Android Studio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,4 +19,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MassageCart(name: String) {
     Text(text = "Hello $name!")
+}
+@Preview
+@Composable
+fun PreviewMassageCart() {
+    MassageCart("Android")
 }
